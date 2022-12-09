26e27745817de7db3b227685bcbb992b1af6ba73
kekeh1 <62773288+kekeh1@users.noreply.github.com>
2022-12-09 12:27:09 -0500

    change

--- PF/pf/src/App.js
+++ PF/pf/src/App.js
@@ -6,7 +6,6 @@ import Studios from './Components/Studios/Studio_All';
 import Studio from './Components/Studios/Studio_Detail';
 import "@progress/kendo-theme-default/dist/all.css";
 import PickDateOfClass from  './Components/Classes/Canlendar'
-import OtherClasses  from  './Components/Classes/OtherSearch'
 import RegisterPage from './Components/Accounts/Register';
 import LoginPage from './Components/Accounts/Login';
 import ProfilePage from './Components/Accounts/Profile';
@@ -21,7 +20,6 @@ function App() {
         <Route path="/Studio/:studio_id" element={<Studio />} />
 
         <Route path="/Date/:studio_id" element={<PickDateOfClass/>} />
-        <Route path="/Other/:studio_id" element={<OtherClasses/>} />
 
         <Route path="/Signup" element={<RegisterPage/>} />
         <Route path="/Login" element={<LoginPage/>} />
