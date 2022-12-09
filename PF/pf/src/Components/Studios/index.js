c27e025d345637f70f58ad64f3cd099ffa33ae93
gracekim15237 <gracekim15237@gmail.com>
2022-12-08 23:03:39 -0500

    Perfect Studio List🥰

--- PF/pf/src/Components/Studios/index.js
+++ PF/pf/src/Components/Studios/index.js
@@ -2,12 +2,13 @@ import React, {useEffect, useState} from "react";
 
 const Studios = () => {
     const [studios, setStudios] = useState(null);
-    const [params, setParams] = useState({page: 1, name: "", coach: "", amenity: ""});
+    const [params, setParams] = useState({page: 1, name: "", coach: "", amenity: "", quantity: ""});
+    const [preps, setPreps] = useState({page: 1, name: "", coach: "", amenity: "", quantity: ""});
     const [total, setTotal] = useState(1);
 
     useEffect(() => {
-        const { page, name, coach, amenity } = params;
-        fetch(`http://127.0.0.1:8000/studios/list/?p=${page}&name=${name}&coach=${coach}&amenity=${amenity}`)
+        const { page, name, coach, amenity, quantity } = params;
+        fetch(`http://127.0.0.1:8000/studios/list/?p=${page}&name=${name}&coach=${coach}&amenity=${amenity}&quantity=${quantity}`)
             .then(res => {
                 const comp = parseInt(res.headers.get('count'));
                 (comp % 5)===0 ? setTotal(Math.round(comp / 5)) : setTotal(Math.round(comp / 5) + 1);
@@ -20,10 +21,10 @@ const Studios = () => {
       <p>Studio Name
          <input
             style={{width: 100, height: 20, fontSize: 18, margin: 4}}
-            value={params.name}
+            value={preps.name}
             onChange={(event) => {
-                setParams({
-                    ...params,
+                setPreps({
+                    ...preps,
                     name: event.target.value,
                     page: 1,
                 })
@@ -32,10 +33,10 @@ const Studios = () => {
       <p>Coach Name
          <input
             style={{width: 100, height: 20, fontSize: 18, margin: 4}}
-            value={params.coach}
+            value={preps.coach}
             onChange={(event) => {
-                setParams({
-                    ...params,
+                setPreps({
+                    ...preps,
                     coach: event.target.value,
                     page: 1,
                 })
@@ -44,19 +45,38 @@ const Studios = () => {
       <p>Amenity
          <input
             style={{width: 100, height: 20, fontSize: 18, margin: 4}}
-            value={params.amenity}
+            value={preps.amenity}
             onChange={(event) => {
-                setParams({
-                    ...params,
+                setPreps({
+                    ...preps,
                     amenity: event.target.value,
                     page: 1,
                 })
             }}
          /></p>
+      <p>Quantity
+         <input
+            style={{width: 100, height: 20, fontSize: 18, margin: 4}}
+            value={preps.quantity}
+            onChange={(event) => {
+                setPreps({
+                    ...preps,
+                    quantity: event.target.value,
+                    page: 1,
+                })
+            }}
+         /></p>
+        <button onClick={(event) => {
+                setParams({
+                    ...params,
+                    name: preps.name,
+                    coach: preps.coach,
+                    amenity: preps.amenity,
+                    quantity: preps.quantity,
+                    page: 1,
+                })
+            }}>Search!</button>
           <br/><br/><>{ total }</>
-        {/*<button onClick={*/}
-
-        {/*  }>Search!</button>*/}
             <table>
             <thead>
             <tr>
@@ -82,14 +102,14 @@ const Studios = () => {
             <button onClick={() => setParams({
                 ...params,
                 page: Math.max(1, params.page - 1)
-            })}>
+            })} disabled={ params.page === 1 }>
                 prev
             </button>
             <>{ params.page }</>
             <button onClick={() => setParams({
                 ...params,
                 page: Math.min(total, params.page + 1)
-            })}>
+            })} disabled={ params.page === total || total === 0 }>
                 next
             </button>
         </>
